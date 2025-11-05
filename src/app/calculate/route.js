14bc1c6c7e7f65139a1d3e70f333e9fe20d78857
unknown <you@example.com>
2025-11-05 21:12:32 +0200

    x+y

--- src/app/calculate/route.js
+++ src/app/calculate/route.js
@@ -1,3 +1,15 @@
-export async function GET() {
-    return Response.json({ message: 'Here will come x + y' })
+export async function GET(req1) {
+
+  const urlData = new URL(req1.url);
+  console.log("urlData = ", urlData);
+  const {searchParams} = urlData;
+  // const searchParams = urlData.searchParams;
+  const x = Number(searchParams.get("x")); 
+  const y = Number(searchParams.get("y")); 
+
+
+    return Response.json({ result: `${x} + ${y} = ${x+y}` })
+
+
+    // return Response.json({ result: `${x} + ${y} = ${x+y}` })
   }
