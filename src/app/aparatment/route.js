3612214e217d65d25b2ac891609680313cfcbbfe
lingar <yimprogramming@gmail.com>
2025-11-26 16:21:23 +0200

    Creating web service apartments

--- src/app/aparatment/route.js
+++ src/app/aparatment/route.js
@@ -1,5 +1,11 @@
 const apartments = [];
 
+let id = 0;
+
+const generateId = () => {
+    return id++;
+}
+
 /*
 o address: string
 o rooms: number
@@ -7,13 +13,13 @@ o floor: number
 o type: string
 o isCommercial: Boolean
 *price: number
-
-
  */
+
 export async function GET(req1) {
 
     if (apartments.length < 3) {
         apartments.push({
+
             address: "Hayezira 13 Petach Tikva",
             rooms: 1,
             floor: 2,
@@ -41,4 +47,32 @@ export async function GET(req1) {
     }
 
     return Response.json(apartments);
+}
+
+export async function POST(req) {
+    const newApartment = req.body;
+    newApartment.id = generateId();
+    apartments.push(newApartment);
+    return Response.json(newApartment);
+
+}
+
+export async function PUT(req) {
+    const updatedApartment = req.body;
+
+    const index = apartments.findIndex(item => item.id === updatedApartment.id);
+    apartments[index] = updatedApartment;
+    return Response.json(updatedApartment);
+
+}
+
+
+export async function DELETE(req) {
+    const id = req.body.id;
+
+    const index = apartments.findIndex(item => item.id === id);
+
+    apartments.splice(index, 1);
+    return Response.json(true);
+
 }
