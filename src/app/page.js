ae1c7477bdacb89f03e1f6d1fffa5d70c49b023b
lingar <yimprogramming@gmail.com>
2025-12-26 11:57:35 +0200

    Milestone - some cleaning

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import Image from "next/image";
-
 import styles from "./page.module.css";
 import Link from "next/link";
 
 
 export default function Home() {
     return (
-        <div className={styles.page}>
+        <div >
 
 
             <div style={{
