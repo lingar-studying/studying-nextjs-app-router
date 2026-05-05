dbb62ca318827db08cd2745092294b5b61054378
lingar <yimprogramming@gmail.com>
2026-05-06 02:36:34 +0300

    Adding context for see it in the inner

--- src/app/pages/docs/client-insights/DataManagementProblem.jsx
+++ src/app/pages/docs/client-insights/DataManagementProblem.jsx
@@ -1,9 +1,10 @@
 import {Box} from "@mui/material";
+import {createContext, useContext} from "react";
 
-const DataManagementProblem =()=>{
+const DataManagementProblem = () => {
 
 
-    return(
+    return (
         <Box component={"div"}>
             <h1>Data Management Problem</h1>
 
@@ -15,23 +16,25 @@ const DataManagementProblem =()=>{
 
             </ol>
 
-            <Box component={"div"}
-            sx = {{
-                display: "flex",
-                flexWrap: "wrap",
-                padding: 3,
-                justifyContent: "space-around",
-                border: "2px solid purple",
-                gap: 2
+            <h2>Solution: inner context</h2>
 
-            }}
+            <Box component={"div"}
+                 sx={{
+                     display: "flex",
+                     flexWrap: "wrap",
+                     padding: 3,
+                     justifyContent: "space-around",
+                     border: "2px solid purple",
+                     gap: 2
+
+                 }}
 
             >
-                {VALUES.map(item=> (<Parent value={ item}>
+                {VALUES.map(item => (<Parent value={item}>
 
                         <Medium> <Child/> </Medium>
                     </Parent>
-                    ))
+                ))
 
                 }
 
@@ -47,30 +50,44 @@ const DataManagementProblem =()=>{
 export default DataManagementProblem;
 
 //inner component
+export const ParentContext = createContext(0);
 
-const Parent = (props)=>{
+const Parent = (props) => {
 
-    return(
-        <Box component={"div"} sx ={{border: "2px solid black",    minWidth: '250px', maxWidth: "400px",
-            aspectRatio: '1 / 1', padding: 3}}>
+
+    return (
+
+
+        <Box component={"div"} sx={{
+            border: "2px solid black", minWidth: '250px', maxWidth: "400px",
+            aspectRatio: '1 / 1', padding: 3
+        }}>
 
             <p>I am Parent Component, <br/>MY VALUE IS : <b>{props.value}</b></p>
             <p>I have here MEDIUM component that must not have props available</p>
 
             <hr/>
-            {props.children}
-        {/*    The above props saved for the inner element of component. <Parent>%HERE IS THE CHILDREN% </Parent>*/}
+
+            {/*We only need to wrap it with context*/}
+
+            <ParentContext.Provider value={props.value}>
+                {props.children}
+
+            </ParentContext.Provider>
+            {/*    The above props saved for the inner element of component. <Parent>%HERE IS THE CHILDREN% </Parent>*/}
 
         </Box>
     )
 
 }
 
-const Medium =(/**Props forbidden here beside children*/{children})=> (
-    <Box component={"div"} sx ={{border: "2px solid black",    width: '250px',
-        aspectRatio: '1 / 1', padding: 3}}>
+const Medium = (/**Props forbidden here beside children*/{children}) => (
+    <Box component={"div"} sx={{
+        border: "2px solid black", width: '250px',
+        aspectRatio: '1 / 1', padding: 3
+    }}>
         <p>I am <b>MEDIUM</b> component, <br/>
-        I can only to pass children from me. No Logic or data here. No Props to get or pass...
+            I can only to pass children from me. No Logic or data here. No Props to get or pass...
         </p>
 
         <hr/>
@@ -80,15 +97,21 @@ const Medium =(/**Props forbidden here beside children*/{children})=> (
     </Box>
 );
 
-const Child = ()=>(
-    <Box component={"div"}>
-        <p>I am the son of the parent.</p>
+const Child = () => {
 
-        <p>I want to get the value from my parent, but <b>I don't know how.</b> </p>
-    </Box>
+    const parentValue = useContext(ParentContext);
 
+    return (
+        <Box component={"div"}>
+            <p>I am the son of the parent.</p>
 
-)
+            <p>I want to get the value from my parent, but <b>I know how. I use context</b></p>
+
+            <p>the value of my parent is: {parentValue}</p>
+        </Box>
+    )
+
+}
 
 const VALUES = [22, 23, 58, 94, 75];
 
