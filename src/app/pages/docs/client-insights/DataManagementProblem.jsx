3ed9cfd0e132ba0a88dd7169133b1ccef68c3d0d
lingar <yimprogramming@gmail.com>
2026-05-06 01:55:45 +0300

    Adding the medium and child

--- src/app/pages/docs/client-insights/DataManagementProblem.jsx
+++ src/app/pages/docs/client-insights/DataManagementProblem.jsx
@@ -27,7 +27,11 @@ const DataManagementProblem =()=>{
             }}
 
             >
-                {VALUES.map(item=> <Parent value={ item}/>)
+                {VALUES.map(item=> (<Parent value={ item}>
+
+                        <Medium> <Child/> </Medium>
+                    </Parent>
+                    ))
 
                 }
 
@@ -47,17 +51,44 @@ export default DataManagementProblem;
 const Parent = (props)=>{
 
     return(
-        <Box component={"div"} sx ={{border: "2px solid black",    width: '250px',
+        <Box component={"div"} sx ={{border: "2px solid black",    minWidth: '250px', maxWidth: "400px",
             aspectRatio: '1 / 1', padding: 3}}>
 
             <p>I am Parent Component, <br/>MY VALUE IS : <b>{props.value}</b></p>
             <p>I have here MEDIUM component that must not have props available</p>
 
+            <hr/>
+            {props.children}
+        {/*    The above props saved for the inner element of component. <Parent>%HERE IS THE CHILDREN% </Parent>*/}
 
         </Box>
     )
 
 }
+
+const Medium =(/**Props forbidden here beside children*/{children})=> (
+    <Box component={"div"} sx ={{border: "2px solid black",    width: '250px',
+        aspectRatio: '1 / 1', padding: 3}}>
+        <p>I am <b>MEDIUM</b> component, <br/>
+        I can only to pass children from me. No Logic or data here. No Props to get or pass...
+        </p>
+
+        <hr/>
+        {children}
+
+
+    </Box>
+);
+
+const Child = ()=>(
+    <Box component={"div"}>
+        <p>I am the son of the parent.</p>
+
+        <p>I want to get the value from my parent, but <b>I don't know how.</b> </p>
+    </Box>
+
+
+)
 
 const VALUES = [22, 23, 58, 94, 75];
 
